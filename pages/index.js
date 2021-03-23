89e3b462b848e5e191b1349861fd60ec48a56310
annamalaigokkul <annamalai.natarajan@askapplications.com>
2021-03-22 22:59:26 -0400

    Title Update

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Gokkul's store
+          Welcome to Gokkul's online store
         </h1>
 
         <p className={styles.description}>
